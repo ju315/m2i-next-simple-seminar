65ed191f1da737b8241ca70e2903b2208efd89be
ByeongJuP <pbju92@gmail.com>
2023-03-03 23:45:34 +0900

    add: document.tsx file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,9 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <meta charSet="utf-8" />
-        <title>Nextjs base</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Component {...pageProps} />
     </>
   );
