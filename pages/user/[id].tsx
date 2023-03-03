07b0e8820039b47d92892237c56e87725fb6b6b1
ByeongJuP <pbju92@gmail.com>
2023-03-03 23:56:22 +0900

    add: user 정보 추가

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -1,14 +1,28 @@
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import faker from 'faker';
 
 const User = () => {
   const router = useRouter();
   const { id } = router.query;
+
+  const [lastName, setLastName] = useState('');
+  const [job, setJob] = useState('');
+
+  useEffect(() => {
+    if (id) {
+      setLastName(faker.name.lastName());
+      setJob(faker.name.jobTitle());
+    }
+  }, [id]);
+
   return (
     <>
       <h1>User Info</h1>
       <div>
-        <h3>User name: {id}</h3>
+        <h3>User FirstName: {id}</h3>
+        <h3>User LastName: {lastName}</h3>
+        <h3>User Job: {job}</h3>
       </div>
     </>
   );
